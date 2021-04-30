0ad7194ed2d465473f49114e330f4669dfe0e9a3
taeroi <fkrkdlsgk@gmail.com>
2021-04-30 17:16:36 +0900

    feat: Add mainView

--- TutorialMusicApp/ViewController.swift
+++ TutorialMusicApp/ViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var trackImageView: UIImageView!
+    @IBOutlet weak var tarckNameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var playPauseButton: UIButton!
+    
+    @IBOutlet weak var playerBlurView: UIView!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
